68bba9e755a29a59c9520ca32d827252174e9d22
User <y0508352872@gmail.com>
2024-12-24 14:18:56 +0200

    Initial commit

--- app/src/main/java/com/yt/myapplication/Welcome.java
+++ app/src/main/java/com/yt/myapplication/Welcome.java
@@ -4,15 +4,13 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -33,36 +31,37 @@ public class Welcome extends AppCompatActivity {
         checkUserNameAndCreateFileIfNeeded();
         
         // דחיית המעבר לאקטיביטי הבא ב-3 שניות
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // יצירת Intent לעבור לאקטיביטי הבא
-                Intent intent = new Intent(Welcome.this, MainActivity.class); // שים את שם הקטיביטי הבא במקום NextActivity
-                startActivity(intent);
-                finish(); // לסיים את הקטיביטי הנוכחי כדי שלא יחזור אליו
-            }
-        }, 2000); // 3000 מילישניות = 3 שניות
+        new Handler().postDelayed(() -> {
+            // יצירת Intent לעבור לאקטיביטי הבא
+            Intent intent = new Intent(Welcome.this, MainActivity.class); // שים את שם הקטיביטי הבא במקום NextActivity
+            startActivity(intent);
+            finish(); // לסיים את הקטיביטי הנוכחי כדי שלא יחזור אליו
+        }, 2000); // 2000 מילישניות = 2 שניות
     }
     @SuppressLint("SetTextI18n")
     private void checkUserNameAndCreateFileIfNeeded() {
         FileManager fileManager = new FileManager(this);
         try {
+            //יוצר אובייקט File שמייצג את הקובץ בספריית הקבצים הפנימיים של האפליקציה, בעזרת הנתיב שניתן ב־getFilesDir().
             File file = new File(getFilesDir(), TOTAL_USER_DATA_NAME);
-            if (!file.exists()) {
-                // אם הקובץ לא קיים, יציג רק בטקסט
+            if (!file.exists()) {//אם הקובץ לא קיים
+                String defaultData = "שם משתמש:\nדפים שנלמדו:\nדפים שנשארו:";//הגדרת סטרינג ברירת מחדל
+                fileManager.writeInternalFile(TOTAL_USER_DATA_NAME, defaultData, false);//כתיבת הברירת מחדל לקובץ
+                // יציג רק בטקסט
                 textViewUserName.setText("ברוך הבא, בחור יקר!");
-            } else {
-                // אם הקובץ קיים, נקרא את תוכן הקובץ
+            } else {//אם הקובץ קיים
+                //  נקרא את תוכן הקובץ
                 List<String> lines = fileManager.readFileLines(TOTAL_USER_DATA_NAME);
-                if (lines.isEmpty()) {
-                    textViewUserName.setText("ברוך הבא, בחור יקר!");
-                } else {
+                if (lines.isEmpty()) {//אם השורות ריקות בקובץ
+                    textViewUserName.setText("ברוך הבא, בחור יקר!");  // יציג רק בטקסט
+                } else {//אם השורות לא ריקות
                     // קריאת שם המשתמש מהשורה הראשונה
-                    for (String line : lines) {
-                        if (line.startsWith(USERNAME_PREFIX)) {
+                    for (String line : lines) {//עבור בלולאה על כל הקובץ
+                        if (line.startsWith(USERNAME_PREFIX)) {//מצא שורה שמחילה בשם משתמש
+                            //חתוך את שם המשתמש מהשורה והסרת רווחים תחילה וסוף ולוקח זאת למשתנה
                             String userName = line.substring(USERNAME_PREFIX.length()).trim();
                             textViewUserName.setText("ברוך הבא, " + userName + "!");
-                            break;
+                            break;//עוצר את הלולאה במידה והשורה נמצאה
                         }
                     }
                 }
